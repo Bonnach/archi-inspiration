8f9848fa8765948ebb9670c2b4c216c6e553f532
Simon Bonnacie <simon.bonnacie@gmail.com>
2025-10-25 20:35:00 +0200

    fix: allow R2 images in Next.js Image Optimization

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-85c68dae5a244ee6abc7105e62067ea7.r2.dev',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
